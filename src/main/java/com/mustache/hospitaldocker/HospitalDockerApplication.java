6d5382e125e4780dae962b94006a324b5ceece03
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-11 11:25:03 +0900

    initial commit

---
+++ src/main/java/com/mustache/hospitaldocker/HospitalDockerApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.hospitaldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/hospitaldocker/HospitalDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.hospitaldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
