508154158ba050b0db0c042058e6ec6170402a1d
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-14 13:34:01 +0900

    실습 - 특정 구의 보건진료소, 보건지소, 보건소 모두 찾기

--- practice.sql
+++ practice.sql
@@ -1,2 +1,5 @@
 SELECT * FROM `likelion-db`.nation_wide_hospitals
 WHERE full_address LIKE '%수원시%' and hospital_name LIKE '%피부%';
+
+SELECT * FROM `likelion-db`.nation_wide_hospitals
+WHERE full_address LIKE '%서대문구%' and hospital_name LIKE '%보건%';
