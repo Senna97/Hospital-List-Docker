46351ceebb90a53aeba66364d5d7025c70c0a82b
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-14 14:16:38 +0900

    특정 구의 보건진료소, 보건지소, 보건소 모두 찾기

--- practice.sql
+++ practice.sql
@@ -1,5 +1,14 @@
-SELECT * FROM `likelion-db`.nation_wide_hospitals
-WHERE full_address LIKE '%수원시%' and hospital_name LIKE '%피부%';
+SELECT *
+FROM `likelion-db`.nation_wide_hospitals
+WHERE full_address LIKE '%수원시%'
+  and hospital_name LIKE '%피부%';
 
-SELECT * FROM `likelion-db`.nation_wide_hospitals
-WHERE full_address LIKE '%서대문구%' and hospital_name LIKE '%보건%';
+SELECT *
+FROM `likelion-db`.nation_wide_hospitals
+WHERE full_address LIKE '%서대문구%'
+  and hospital_name LIKE '%보건%';
+
+SELECT *
+FROM `likelion-db`.nation_wide_hospitals
+WHERE business_type_name in ('보건소', '보건지소', '보진료소')
+  and road_name_address LIKE '%서대문구%';
