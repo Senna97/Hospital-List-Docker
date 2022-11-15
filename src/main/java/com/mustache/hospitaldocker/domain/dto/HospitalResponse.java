2fcc95ff6cdc50e140a24ec91fd6241642ab33e6
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-15 15:28:07 +0900

    @Builder 추가

--- src/main/java/com/mustache/hospitaldocker/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/hospitaldocker/domain/dto/HospitalResponse.java
@@ -1,11 +1,14 @@
 package com.mustache.hospitaldocker.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class HospitalResponse {
     private Integer id;
     private String roadNameAddress;
