f1d1876ba569e3594e0ec5f7bb8323b2bec7d4d1
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-16 11:45:01 +0900

    add ArticleRestController

---
+++ src/main/java/com/mustache/hospitaldocker/controller/ArticleRestController.java
@@ -0,0 +1,27 @@
+package com.mustache.hospitaldocker.controller;
+
+
+import com.mustache.hospitaldocker.domain.dto.ArticleDto;
+import com.mustache.hospitaldocker.service.ArticleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+public class ArticleRestController {
+
+    private final ArticleService articleService;
+
+    public ArticleRestController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDto> getArticle(@PathVariable Long id) {
+        ArticleDto articleDto = articleService.getArticle(id);
+        return ResponseEntity.ok().body(articleDto);
+    }
+}
