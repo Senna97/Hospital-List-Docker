d69ead48e1bcdc05e21c68632061c2a09c242653
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-16 11:43:58 +0900

    add ArticleService

--- src/main/java/com/mustache/hospitaldocker/service/ArticleService.java
+++ src/main/java/com/mustache/hospitaldocker/service/ArticleService.java
@@ -1,11 +1,25 @@
 package com.mustache.hospitaldocker.service;
 
+import com.mustache.hospitaldocker.domain.dto.ArticleDto;
+import com.mustache.hospitaldocker.domain.entity.Article;
 import com.mustache.hospitaldocker.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
 
     public ArticleService(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
+    }
+
+    public ArticleDto getArticle(Long id) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        Article article = optionalArticle.get();
+        ArticleDto articleDto = Article.of(article);
+
+        return articleDto;
     }
 }
