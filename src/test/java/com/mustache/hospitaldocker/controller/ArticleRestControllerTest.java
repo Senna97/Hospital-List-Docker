70fc0d0678d7bb7007660fac6839c18030bee4c7
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-16 11:43:21 +0900

    한 개의 게시글을 조회하는 기능 TDD

--- src/test/java/com/mustache/hospitaldocker/controller/ArticleRestControllerTest.java
+++ src/test/java/com/mustache/hospitaldocker/controller/ArticleRestControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,13 +27,15 @@ class ArticleRestControllerTest {
     @Test
     @DisplayName("한 개의 게시글을 조회하는 기능")
     void view() throws Exception {
-        ArticleDto articleDto = new ArticleDto(1L, "first title", "first content ~ Blah Blah Blah");
-
         Long id = 1L;
-        String url = String.format("/api/v1/articles/%d", id);
-        mockMvc.perform(get(url))
+
+        given(articleService.getArticle(id)).willReturn(new ArticleDto(1L, "first title", "first content ~ Blah Blah Blah"));
+
+        mockMvc.perform(get("/api/v1/articles/1"))
                 .andExpect(jsonPath("$.title").value("first title"))
                 .andExpect(jsonPath("$.content").value("first content ~ Blah Blah Blah"))
                 .andDo(print());
+
+        verify(articleService).getArticle(id);
     }
 }
